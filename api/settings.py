495a6a598736bb7cb0b1f1be021e7e4517b698d0
Ashlesha39 <ashlesha.hbgadget@gmail.com>
2024-08-27 18:11:58 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -521,7 +521,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 #ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
-ALLOWED_HOSTS = ['sales-4-2s0i.onrender.com, 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sales-5-2s0i.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
